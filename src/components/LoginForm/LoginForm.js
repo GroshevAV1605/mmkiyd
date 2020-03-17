467a8b8ee3e3cb7a9df8704230dc4a6d538ff294
anton <groshev.av1605@gmail.com>
2020-03-17 17:36:34 +0500

    fix

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { data } from "../../constants";
+import { data } from "../../constants/AMconstants";
 
 const LoginForm = ({ isLoginChange, usernameChange }) => {
   const [loginData, loginDataChange] = useState({
